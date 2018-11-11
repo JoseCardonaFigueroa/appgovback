e132a496316e9eeba5cc413a161891cb014d6329
Bárbara <barbara@MacBook-Pro-de-Barbara.local>
2018-11-11 12:43:17 -0600

    Changed obras and  progreso seeders so both records are related

--- database/seeds/ObrasTableSeeder.php
+++ database/seeds/ObrasTableSeeder.php
@@ -16,19 +16,55 @@ class ObrasTableSeeder extends Seeder
 
       $faker = \Faker\Factory::create();
 
+      $localidad =  [
+      '1' => 'Aguascalientes',
+      '2' => 'Baja California',
+      '3' => 'Baja California Sur',
+      '4' => 'Campeche',
+      '5' => 'Chiapas',
+      '6' => 'Chihuahua',
+      '7' => 'Coahuila de Zaragoza',
+      '8' => 'Colima',
+      '9' => 'Ciudad de México',
+      '10' => 'Durango',
+      '11' => 'Guanajuato',
+      '12' => 'Guerrero',
+      '13' => 'Hidalgo',
+      '14' => 'Jalisco',
+      '15' => 'Mexico',
+      '16' => 'Michoacan de Ocampo',
+      '17' => 'Morelos',
+      '18' => 'Nayarit',
+      '19' => 'Nuevo Leon',
+      '20' => 'Oaxaca',
+      '21' => 'Puebla',
+      '22' => 'Queretaro de Arteaga',
+      '23' => 'Quintana Roo',
+      '24' => 'San Luis Potosi',
+      '25' => 'Sinaloa',
+      '26' => 'Sonora',
+      '27' => 'Tabasco',
+      '28' => 'Tamaulipas',
+      '29' => 'Tlaxcala',
+      '30' => 'Veracruz-Llave',
+      '31' => 'Yucatan',
+      '32' => 'Zacatecas',
+
+    ];
+
       // And now, let's create a few articles in our database:
-      for ($i = 0; $i < 50; $i++) {
-          Obra::create([
-              'archivo' => $faker->sentence,
+      for ($i = 1; $i <= 30; $i++) {
+          $o = Obra::create([
+              'archivo' => $i,
               'gaveta' => $faker->sentence,
-              'expediente' => $faker->sentence,
+              'expediente' => 200 + $i,
               'gaveta' => $faker->sentence,
-              'no_obra' => $faker->sentence,
-              'registro_de_oficios' => $faker->sentence,
-              'fuente_financiera' => $faker->sentence,
-              'ejercicio' => $faker->sentence,
+              'no_obra' => 'Vo.Bo.-00' + $i + '/1',
+              'registro_de_oficios' => 'oficio',
+              'fuente_financiera' => 'Banamex',
+              'ejercicio' => '2018',
               'descripcion_obra' => $faker->paragraph,
-              'localidad' => $faker->sentence,
+              'localidad' => $localidad[$i],
               'municipio' => $faker->sentence,
               'aprobado' => $faker->sentence,
               'contratado' => $faker->sentence,
@@ -44,6 +80,15 @@ class ObrasTableSeeder extends Seeder
               'termino_programado' => $faker->date($format = 'Y-m-d', $max = 'now'),
 
           ]);
+
+          for ($j=1; $j < 5; $j++) { 
+            Progreso::create([
+              'fecha' => $faker->date($format = 'Y-m-d', $max = 'now'),
+              'porcentaje' => $faker->randomNumber($nbDigits = 100, $strict = false),
+              'dia' => $faker->randomNumber($nbDigits = 100, $strict = false),
+              'subtotal' => $o->id,
+            ]);
+          }
       }
     }
 }
